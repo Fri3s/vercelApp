d3a6338a11907faad668045b094bcb2585947252
Fri3s <lxie531@gmail.com>
2024-04-13 14:31:07 -0700

    Fix CORS

--- server.js
+++ server.js
@@ -1,21 +1,9 @@
 // Install the necessary packages: npm install express cors node-fetch
 import express from 'express';
-import cors from 'cors';
 import fetch from 'node-fetch';
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-const allowedOrigins = ['https://burgerfri3s-5c9f2.web.app'];
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
 
 app.get('/fetchWebsite', async (req, res) => {
   const url = req.query.url;
